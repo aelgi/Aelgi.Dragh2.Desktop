f74d54fa53aa8182a9b3f84d2007de755508ae37
sanchez <dfitz360@gmail.com>
2019-09-30 21:49:23 +1000

    Added a single static player image

--- Aelgi.Dragh2.Core/Entities/Player.cs
+++ Aelgi.Dragh2.Core/Entities/Player.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using Aelgi.Dragh2.Core.Enums;
+﻿using Aelgi.Dragh2.Core.Enums;
 using Aelgi.Dragh2.Core.IModels;
 using Aelgi.Dragh2.Core.IServices;
+using Aelgi.Dragh2.Core.Models;
 
 namespace Aelgi.Dragh2.Core.Entities
 {
@@ -9,16 +9,23 @@ namespace Aelgi.Dragh2.Core.Entities
     {
         public static int MoveSpeed = 1;
 
+        protected Position _drawPosition;
+
+        protected string _imageName => "Player";
+
         public void Update(IGameUpdateService gameService)
         {
             if (gameService.IsPressed(Key.UP)) gameService.GamePosition.Y -= MoveSpeed;
             if (gameService.IsPressed(Key.DOWN)) gameService.GamePosition.Y += MoveSpeed;
             if (gameService.IsPressed(Key.LEFT)) gameService.GamePosition.X -= MoveSpeed;
             if (gameService.IsPressed(Key.RIGHT)) gameService.GamePosition.X += MoveSpeed;
+
+            _drawPosition = gameService.WindowSize / 2;
         }
 
         public void Render(IGameRenderService gameService)
         {
+            gameService.DrawImage(_drawPosition, _imageName);
         }
     }
 }
